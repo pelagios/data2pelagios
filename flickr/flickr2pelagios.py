4932a03acaafda85b2deee66fcf8bcc34fcb4051
Rainer Simon <rainer.simon@ait.ac.at>
2014-10-05 16:41:06 +0200

    Minor edits

--- flickr/flickr2pelagios.py
+++ flickr/flickr2pelagios.py
@@ -4,8 +4,19 @@ with open('api-key.txt', 'r') as f:
   api_key = f.readline().strip()
   
   print('API key: ' + api_key)
-
+  
   flickr = flickrapi.FlickrAPI(api_key)
 
+  output = open('pef-pelagios.ttl', 'w')
+  output.write('@prefix dcterms: <http://purl.org/dc/terms/> .\n')  
+  output.write('@prefix foaf: <> . \n')
+  output.write('@prefix pelagios: <http://pelagios.github.io/vocab/terms#> .\n')
+  output.write('@prefix oa: <http://www.w3.org/ns/oa#> .\n\n')
+
   for photo in flickr.walk(user_id = 'palestineexplorationfund'):
-    print photo.get('title')
+    output.write('<http://example.org/pef/dump.ttl#images/' + photo.get('id') + '> a pelagios:AnnotatedThing ;\n')
+    output.write('  dcterms:title "' + photo.get('title').encode('utf-8') + '" ;\n');  
+    output.write('  foaf:homepage <https://farm' + photo.get('farm') + '.staticflickr.com/' + photo.get('server') + '/' + photo.get('id') + '_' + photo.get('secret') + '.jpg> ;\n')   
+    output.write('  .\n\n')
+
+
