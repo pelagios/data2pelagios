6ec108c6bc7375dfc8951435ce1a49fc7b68cebd
Rainer Simon <rainer.simon@ait.ac.at>
2014-10-07 21:30:37 +0200

    Made things a bit more scriptable

--- flickr/flickr2pelagios.py
+++ flickr/flickr2pelagios.py
@@ -1,51 +1,63 @@
+import sys
 import flickrapi
 
-with open('api-key.txt', 'r') as f:
-  api_key = f.readline().strip()
-  user_id = 'palestineexplorationfund'
+with open('api-key.txt', 'r') as key_file:
+  api_key = key_file.readline().strip()
     
-  flickr = flickrapi.FlickrAPI(api_key)
-
-  output = open('pef-pelagios.ttl', 'w')
+  if len(sys.argv) < 3:
+	  print('Missing parameters. Usage: \'python flickr2pelagios.py <user-id> <title>\'')
+	  sys.exit()
+	  
+  user_id = sys.argv[1]
+  stream_title = sys.argv[2]	  
   
-  output.write('@prefix dcterms: <http://purl.org/dc/terms/> .\n')  
-  output.write('@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n')
-  output.write('@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .\n')
-  output.write('@prefix oa: <http://www.w3.org/ns/oa#> .\n')
-  output.write('@prefix pelagios: <http://pelagios.github.io/vocab/terms#> .\n\n')
-
-  ctr_all = 0
-  ctr_with_pleiades = 0
+  print('Harvesting \'' + stream_title + '\' (' + user_id + ')')
   
-  for photo in flickr.walk(user_id = user_id, extras = 'machine_tags,date_taken,geo'):
-    ctr_all += 1
-    photo_id = photo.get('id')
-    pleiades_tags = [ tag for tag in photo.get('machine_tags').split() if tag.startswith('pleiades') ]
+  # Write VoID
+  with open('flickr.void.ttl.template') as void_template:
+    void = open(user_id + '.void.ttl', 'w')
+    void.write(void_template.read().replace('@@title@@', stream_title).replace('@@userID@@', user_id))
+    void.close()
+    
+    # Write annotations
+    flickr = flickrapi.FlickrAPI(api_key)
+    annotations = open(user_id + '.ttl', 'w')
+    annotations.write('@prefix dcterms: <http://purl.org/dc/terms/> .\n')  
+    annotations.write('@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n')
+    annotations.write('@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .\n')
+    annotations.write('@prefix oa: <http://www.w3.org/ns/oa#> .\n')
+    annotations.write('@prefix pelagios: <http://pelagios.github.io/vocab/terms#> .\n\n')
 
-    if len(pleiades_tags) > 0:
-      ctr_with_pleiades += 1
-      output.write('<http://example.org/flickr/dump.ttl#photos/' + photo_id + '> a pelagios:AnnotatedThing ;\n')
-      output.write('  dcterms:title "' + photo.get('title').replace('"', '\\"').encode('utf-8') + '" ;\n');  
-      output.write('  foaf:homepage <http://www.flickr.com/photos/' + user_id + '/' + photo_id + '> ;\n')
+    ctr_all = 0
+    ctr_with_pleiades = 0
+  
+    for photo in flickr.walk(user_id = user_id, extras = 'machine_tags,date_taken,geo'):
+      ctr_all += 1
+      photo_id = photo.get('id')
+      pleiades_tags = [ tag for tag in photo.get('machine_tags').split() if tag.startswith('pleiades') ]
+
+      if len(pleiades_tags) > 0:
+        ctr_with_pleiades += 1
+        annotations.write('<http://example.org/flickr/' + user_id + '.ttl#photos/' + photo_id + '> a pelagios:AnnotatedThing ;\n')
+        annotations.write('  dcterms:title "' + photo.get('title').replace('"', '\\"').encode('utf-8') + '" ;\n');  
+        annotations.write('  foaf:homepage <http://www.flickr.com/photos/' + user_id + '/' + photo_id + '> ;\n')
       
-      # Note: image at 'https://farm' + photo.get('farm') + '.staticflickr.com/' + photo.get('server') + '/' + photo.get('id') + '_' + photo.get('secret') + '.jpg'   
-
-      if photo.get('datetaken'):
-        year = photo.get('datetaken').split('-', 1)[0]
-        output.write('  dcterms:temporal "start=' + year + '" ;\n')
+        if photo.get('datetaken'):
+          year = photo.get('datetaken').split('-', 1)[0]
+          annotations.write('  dcterms:temporal "start=' + year + '" ;\n')
         
-      if photo.get('latitude') and photo.get('longitude'):
-        output.write('  dcterms:spatial [ geo:lat ' + photo.get('latitude') + ' ; geo:long ' + photo.get('longitude') + ' ] ;\n')
+        if photo.get('latitude') and photo.get('longitude'):
+          annotations.write('  dcterms:spatial [ geo:lat ' + photo.get('latitude') + ' ; geo:long ' + photo.get('longitude') + ' ] ;\n')
         
-      output.write('  .\n\n')
+        annotations.write('  .\n\n')
       
-      for idx, pleiades_tag in enumerate(pleiades_tags):  
-        output.write('<http://example.org/flickr/dump.ttl#photos/' + photo_id + '/annotations/' + str(idx) + '> a oa:Annotation ;\n')
-        output.write('  oa:hasTarget <http://example.org/flickr/dump.ttl#photos/' + photo_id + '> ;\n')
-        output.write('  oa:hasBody <http://pleiades.stoa.org/places/' + pleiades_tag[17:] + '> ;\n')
-        output.write('  .\n\n')
+        for idx, pleiades_tag in enumerate(pleiades_tags):  
+          annotations.write('<http://example.org/flickr/' + user_id + '.ttl#photos/' + photo_id + '/annotations/' + str(idx) + '> a oa:Annotation ;\n')
+          annotations.write('  oa:hasTarget <http://example.org/flickr/dump.ttl#photos/' + photo_id + '> ;\n')
+          annotations.write('  oa:hasBody <http://pleiades.stoa.org/places/' + pleiades_tag[17:] + '> ;\n')
+          annotations.write('  .\n\n')
       
-  output.close()
+    annotations.close()
   
-  print('Processed ' + str(ctr_all) + ' photos')
-  print('Got ' + str(ctr_with_pleiades) + ' with Pleiades tag')
+    print('Processed ' + str(ctr_all) + ' photos')
+    print('Got ' + str(ctr_with_pleiades) + ' with Pleiades tag')
---
+++ flickr/harvest.sh
@@ -0,0 +1,6 @@
+python flickr2pelagios.py palestineexplorationfund 'The Palestine Exploration Fund'
+python flickr2pelagios.py carolemage 'Following Hadrian'
+python flickr2pelagios.py dandiffendale 'Dan Diffendale'
+python flickr2pelagios.py dankate 'Daniel and Kate'
+python flickr2pelagios.py APAAME 'Aerial Photographic Archive for Archaeology in the Middle East'
+python flickr2pelagios.py 99690453@N02 'Sarah E. Bond'
