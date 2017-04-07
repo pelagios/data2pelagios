d087dfe5510998e5793cc097facf14eb69930605
Rainer Simon <rainer.simon@ait.ac.at>
2017-04-07 11:44:33 +0200

    Minor edits

--- gazetteer/portolans/rewrite_annotations.rb
+++ gazetteer/portolans/rewrite_annotations.rb
@@ -3,15 +3,25 @@ require 'csv'
 input_file = ARGV
 
 def load_concordances
+  concordances = {}
+
   csv_text = File.read('portolan_precursors_recogito.csv')
   csv = CSV.parse(csv_text, :headers => true, :col_sep => ';')
   csv.each do |row|
-    # TODO implement
+    maphist_id = row[0]
+    mapped_id  = row[2]
+    status     = row[6]
+
+    if status == 'VERIFIED' && !mapped_id.empty?
+      puts(maphist_id + ' -> ' + mapped_id)
+    end
   end
+
+  concordances
 end
 
-def rewrite_annotations
+def rewrite_annotations(concordances)
 end
 
-load_concordances()
-rewrite_annotations()
+concordances = load_concordances()
+rewrite_annotations(concordances)
